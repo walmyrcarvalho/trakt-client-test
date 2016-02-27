e46f3bc5590dee4b9be6304e3cf2f2f73d9d8f95
Walmyr Carvalho <walmyrubuntu@gmail.com>
2016-02-27 16:00:59 -0300

    First commit, clean project

---
+++ app/src/main/java/me/walmyrcarvalho/recrutamentoandroid/ShowActivity.java
@@ -0,0 +1,13 @@
+package me.walmyrcarvalho.recrutamentoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+    }
+}
