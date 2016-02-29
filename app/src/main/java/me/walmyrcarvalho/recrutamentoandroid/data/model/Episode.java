9771b1f4303358e75bbea5c3d7c02480b8f7d221
Walmyr Carvalho <walmyrubuntu@gmail.com>
2016-02-29 09:32:13 -0300

    Updated model classes with Parcelable support

--- app/src/main/java/me/walmyrcarvalho/recrutamentoandroid/data/model/Episode.java
+++ app/src/main/java/me/walmyrcarvalho/recrutamentoandroid/data/model/Episode.java
@@ -1,10 +1,17 @@
 package me.walmyrcarvalho.recrutamentoandroid.data.model;
 
+import org.parceler.Parcel;
+import org.parceler.ParcelConstructor;
+
 import me.walmyrcarvalho.recrutamentoandroid.data.model.collections.Ids;
 
+@Parcel
 public class Episode {
     public int season, number;
 
     public String title;
     public Ids ids;
+
+    @ParcelConstructor
+    public Episode() { /**/ }
 }
--- app/src/main/java/me/walmyrcarvalho/recrutamentoandroid/data/model/collections/Ids.java
+++ app/src/main/java/me/walmyrcarvalho/recrutamentoandroid/data/model/collections/Ids.java
@@ -1,7 +1,14 @@
 package me.walmyrcarvalho.recrutamentoandroid.data.model.collections;
 
+import org.parceler.Parcel;
+import org.parceler.ParcelConstructor;
+
+@Parcel
 public class Ids {
     public int trakt, tvdb, tmdb, tvrage;
 
     public String imdb, slug;
+
+    @ParcelConstructor
+    public Ids() { /**/ }
 }
