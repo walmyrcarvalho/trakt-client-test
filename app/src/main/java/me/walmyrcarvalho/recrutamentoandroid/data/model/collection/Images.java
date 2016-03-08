c899c909d0c46d8a78ac2fed2944f5793bd83b69
Walmyr Carvalho <walmyrubuntu@gmail.com>
2016-03-08 05:15:27 -0300

    Removal of unused entities

--- app/src/main/java/me/walmyrcarvalho/recrutamentoandroid/data/model/collection/Images.java
+++
@@ -1,7 +0,0 @@
-package me.walmyrcarvalho.recrutamentoandroid.data.model.collection;
-
-import me.walmyrcarvalho.recrutamentoandroid.data.model.Image;
-
-public class Images {
-    Image fanart, poster, logo, clearart, banner, thumb;
-}
