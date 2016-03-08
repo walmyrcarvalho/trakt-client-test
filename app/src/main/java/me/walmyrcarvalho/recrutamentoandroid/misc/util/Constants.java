d5977cb79327c2ebb3d6c2c60e9563e13106923a
Walmyr Carvalho <walmyrubuntu@gmail.com>
2016-03-08 14:36:27 -0300

    Loading API key from .properties file

--- app/src/main/java/me/walmyrcarvalho/recrutamentoandroid/misc/util/Constants.java
+++ app/src/main/java/me/walmyrcarvalho/recrutamentoandroid/misc/util/Constants.java
@@ -15,9 +15,7 @@ public class Constants {
 
     // NOTE: This is not the ideal approach for saving API keys on Android. Almost no one is safe,
     // but there's some ways to make it hard to get like have a custom .so or use NDK.
-
     public static final String VALUE_API_KEY       = BuildConfig.API_KEY;
-//    public static final String VALUE_API_KEY       = "3d6382af88e858392b7e1d2aab95fffbb941756452abd42977463c809bbbf36e";
     public static final String VALUE_API_VERSION   = "2";
     public static final String VALUE_CONTENT_TYPE  = "application/json";
 
